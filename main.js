958529374af1f28ae8a08c9d6f6145025f82e59e
CreativeMind012 <88550031+CreativeMind012@users.noreply.github.com>
2022-04-07 18:46:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-a = 1;
+m1 = "";
+m2 = "";
+
+function preload(){
+    m1 = loadSound("music.mp3");
+    m2 = loadSound("music2.mp3");
+}
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = captureVideo(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 600, 500);
+}

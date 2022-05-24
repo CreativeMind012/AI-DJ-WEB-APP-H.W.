913f8f39d50c87c6bfbdd8f4ae3ee62df177a4dc
CreativeMind012 <88550031+CreativeMind012@users.noreply.github.com>
2022-05-24 18:47:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ rightW_Y = 0;
 score_LeftW = 0;
 score_RightW = 0;
 
+m1_status = ""; 
+
 function preload(){
     m1 = loadSound("music.mp3");
     m2 = loadSound("music2.mp3");
@@ -47,8 +49,34 @@ function gotPoses(results)
 		console.log("leftWristX = " + leftW_X + "leftWristY = " + leftW_Y);
     }
 }
-/*
-*/
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    m1_status = m1.isPlaying();     
+
+    fill("#FF0000");
+	stroke("#FF0000");
+
+    if(score_LeftW > 0.2){
+        circle(leftW_X, leftW_Y, 20);
+        m2.stop();
+
+		if(m1_status == false)
+		{
+            m1_status = m1.isPlaying();
+			document.getElementById("song_name").innerHTML = "Song Playing: ...";
+		}
+    }
 }
+/*
+(song1 will be played when left wrist is shown);
+    if(score_RightW > 0.2){
+        circle(rightW_X, rightW_Y, 20);
+
+		if()
+		{
+            
+			document.getElementById("song_name").innerHTML = "Song Playing: ...";
+		}
+    }
+*/
